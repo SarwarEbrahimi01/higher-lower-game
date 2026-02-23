b994052aedf0ed5d5d313018624cfc3fe3206da9
Sarwar Ebrahimi <sarwarebrahimi01@gmail.com>
2026-02-23 09:34:08 +0430

    The data of the follower counts of the social media celebrities

---
+++ game_data.py
@@ -0,0 +1,76 @@
+# data = [
+#     {
+#         'name': 'Instagram',
+#         'follower_count': '346',
+#         'description': 'Social medial platform',
+#         'country': 'United States'
+#     },
+#     {
+#         'name': 'Cristiano Ronaldo',
+#         'follower_count': '215',
+#         'description': 'Footballer',
+#         'country': 'Portugal'
+#     },
+#     {
+#         'name': 'Ariana Grande',
+#         'follower_count': '183',
+#         'description': 'Musician and actress',
+#         'country': 'United States'
+#     },
+#     {
+#         'name': 'Dwayne Johnson',
+#         'follower_count': '181',
+#         'description': 'Actor and professional wrestler',
+#         'country': 'United States'
+#     },
+#     {
+#         'name': 'Selena Gomez',
+#         'follower_count': '174',
+#         'description': 'Musicion and Actress',
+#         'country': 'United States'
+#     },
+#     {
+#         'name': 'Kylie Jenner',
+#         'follower_count': '172',
+#         'description': 'Reality TV personality and businesswoman and Self-Made Billionaire',
+#         'country': 'United States'
+#     },
+#     {
+#         'name': 'Kim Kardashian',
+#         'follower_count': '167',
+#         'description': 'Reality TV personality and businesswoman',
+#         'country': 'United States'
+#     },
+#     {
+#         'name': 'Lionel Messi',
+#         'follower_count': '149',
+#         'description': 'Footballer',
+#         'country': 'Argentina'
+#     },
+#     {
+#         'name': 'Beyonce',
+#         'follower_count': '145',
+#         'description': 'Musician',
+#         'country': 'United States'
+#     },
+#     {
+#         'name': 'Neymar',
+#         'follower_count': '138',
+#         'description': 'Footballer',
+#         'country': 'Brasil'
+#     },
+#
+# ]
+
+data = [
+    { 'name': 'Instagram', 'follower_count': '750+', 'description': 'Social media platform', 'country': 'United States' },
+    { 'name': 'Cristiano Ronaldo', 'follower_count': '670+', 'description': 'Footballer', 'country': 'Portugal' },
+    { 'name': 'Lionel Messi', 'follower_count': '500+', 'description': 'Footballer', 'country': 'Argentina' },
+    { 'name': 'Selena Gomez', 'follower_count': '420+', 'description': 'Musician and Actress', 'country': 'United States' },
+    { 'name': 'Kylie Jenner', 'follower_count': '390+', 'description': 'Reality TV personality and businesswoman', 'country': 'United States' },
+    { 'name': 'Dwayne Johnson', 'follower_count': '390+', 'description': 'Actor and professional wrestler', 'country': 'United States' },
+    { 'name': 'Ariana Grande', 'follower_count': '370+', 'description': 'Musician and actress', 'country': 'United States' },
+    { 'name': 'Kim Kardashian', 'follower_count': '350+', 'description': 'Reality TV personality and businesswoman', 'country': 'United States' },
+    { 'name': 'Beyonce', 'follower_count': '310+', 'description': 'Musician', 'country': 'United States' },
+    { 'name': 'Neymar', 'follower_count': '220+', 'description': 'Footballer', 'country': 'Brasil' },
+]
